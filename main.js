337e5899428ea6ee42338915fa8be751c9bf8161
Hughes, Mark <hughesm@ryanair.com>
2020-03-20 20:06:51 +0000

    firebase details

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-// Initialize Firebase (ADD YOUR OWN DATA)
-var config = {
-  apiKey: "xxxxx",
-  authDomain: "xxxxx",
-  databaseURL: "xxxxx",
-  projectId: "xxxxx",
-  storageBucket: "xxxxx",
-  messagingSenderId: "xxxxx"
-};
-firebase.initializeApp(config);
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBV9OWhiTQ0_mSQRIOys9_03PCiC2RZraw",
+    authDomain: "gorkville.firebaseapp.com",
+    databaseURL: "https://gorkville.firebaseio.com",
+    projectId: "gorkville",
+    storageBucket: "gorkville.appspot.com",
+    messagingSenderId: "481238572240",
+    appId: "1:481238572240:web:13996d01106a9939e552d5",
+    measurementId: "G-PEPNRECKR3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 // Reference messages collection
 var messagesRef = firebase.database().ref('messages');
